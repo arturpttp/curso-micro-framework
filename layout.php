ad0e413b8a59eb7c58c21977c0faecb8d8e8522c
arturpttp <36490387+arturpttp@users.noreply.github.com>
2019-08-12 15:32:19 -0300

    Create layout.php

---
+++ layout.php
@@ -0,0 +1,24 @@
+<?php ?>
+
+<!doctype html>
+<html lang="pt">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS  -->
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- JS  -->
+    <script src="/public/assets/js/jquery.min.js" ></script>
+    <script src="https://kit.fontawesome.com/3f84c41d86.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Site  -->
+    <title><?php echo \Core\Settings::$title; ?></title>
+</head>
+<body>
+    <?php $this->content(); ?>
+xc
+</body>
+</html>
